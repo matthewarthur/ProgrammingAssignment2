9faf4d8ce6ee2d32619937ab9b6e7b13b1dd5ae0
matthewarthur <matthewarthur@matthews-imac.home>
2014-11-19 19:41:58 -0500

    RProg-9 Assignment 2
    Assignment requiring use of functions involving lexical scoping.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix.R contains two functions that create a matrix and determine its inverse
+## with a statement indicating if the value is cached before returning results.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates and sets default values for matrix and solution
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function( m = matrix() ) {
+  i <- NULL
+  
+  set <- function(matrix) {
+    m <<- matrix
+    i <<- NULL
+  }
+
+  get <- function() {m}
+  
+  setInverse <- function(inverse) {i <<- inverse}
+  
+  getInverse <- function() {i}
+  
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve checks if the solution to a matrix is cached and indicates before returning
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x' 
+  m <- x$getInverse()
+  
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get() 
+  m <- solve(data) %*% data  ## syntax for finding matrix inverse
+  x$setInverse(m)
+  
+  m
+  
 }
+
